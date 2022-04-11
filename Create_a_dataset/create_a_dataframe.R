dd908c824022419af76041e4e8a533fb247edbe5
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-11 00:28:46 -0500

    Create create_a_dataframe.R

---
+++ Create_a_dataset/create_a_dataframe.R
@@ -0,0 +1,6 @@
+patientID <- c(1,2,3,4)
+age <- c(25,34,28,52)
+diabetes <- c("type1", "type2", "type1", "type1")
+status <- c("poor", "improved", "excellent", "poor")
+patientdata <- data.frame(patientID, age, diabetes, status)
+patientdata
