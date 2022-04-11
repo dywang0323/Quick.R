5465c0f1d2aa1033ec86ad6fd8f66bddf0eeeb30
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-11 00:37:02 -0500

    Update create_a_dataframe.R

--- Create_a_dataset/create_a_dataframe.R
+++ Create_a_dataset/create_a_dataframe.R
@@ -1,6 +1,17 @@
+# create a dataframe
+
 patientID <- c(1,2,3,4)
 age <- c(25,34,28,52)
 diabetes <- c("type1", "type2", "type1", "type1")
 status <- c("poor", "improved", "excellent", "poor")
 patientdata <- data.frame(patientID, age, diabetes, status)
 patientdata
+
+# select elements in the dataframe
+
+patientdata[1:2]
+patientdata[c("diabetes", "status")]
+
+# generate a list between two variables
+
+table <- (patientdata$diabets, patientdata$status)
