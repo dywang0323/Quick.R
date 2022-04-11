9f04905b3fd04227de19de4f39556fc452b53ac1
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-11 01:36:43 -0500

    Update create_a_list.R

--- Create_a_dataset/create_a_list.R
+++ Create_a_dataset/create_a_list.R
@@ -1,1 +1,8 @@
-
+g <- "my first list"
+h <- c(25,26,18,39)
+j <- matrix (1:10, nrow=5)
+k <- c("one", "two", "three")
+mylist <- list (title = g, age =h, j, k)
+mylist
+mylist[[2]]
+mylist[["agge"]]
