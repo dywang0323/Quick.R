05affa4248dd169ef49ba6a28010a3576c05999b
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-04-11 01:24:01 -0500

    Update usage_of_factor.R

--- Create_a_dataset/usage_of_factor.R
+++ Create_a_dataset/usage_of_factor.R
@@ -1,1 +1,9 @@
-
+patientID <- c(1,2,3,4)
+age <- c(25, 34, 28, 52)
+diabetes <- c("type1", "type2", "type1", "type1")
+status <- c("poor", "improved", "excellent", "poor")
+diabetes <- factor(diabetes)
+status <- factor (status)
+patientdata <- data.ftame(patientID, age, diabetes, status)
+str(patientdata)
+summary(patientdata)
