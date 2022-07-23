37dac3d098ea30b5733e79018cf6f3b1f5e473ac
Dongyu Wang <60108209+dywang0323@users.noreply.github.com>
2022-07-23 16:41:58 -0500

    Update create_a_array.R

--- Create_a_dataset/create_a_array.R
+++ Create_a_dataset/create_a_array.R
@@ -1,4 +1,4 @@
 dim1 <- c("A1", "A2")
 dim2 <- c("B1", "B2", "B3")
-dim3 <- C("c1", "c2", "c3", "c4")
+dim3 <- c("c1", "c2", "c3", "c4")
 z <- array (1:24, c(2,3,4), dimnames = list(dim1, dim2, dim2))
